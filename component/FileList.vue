3dd17c86f27ef9b337f2759b5af8f7c4bfabba9a
TianG <18232025994@139.com>
2024-04-14 00:13:42 +0800

    Added My file page and home page

---
+++ component/FileList.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <div class="file-cards">
+        <div v-for="file in files" :key="file.id" class="file-card">
+          <div class="file-info">
+            <img class="file-img" src="../public/我的文件.png" alt="File Image">
+            <hr class="file-divider"> <!-- 添加横杠 -->
+            <h3>{{ file.name }}</h3>
+            <p>大小: {{ file.size }}</p>
+            <p>日期: {{ file.date }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'FileList',
+    data() {
+      return {
+        files:  [
+    { id: 1, name: 'Report.pdf', size: '1.2 MB', date: '2024-01-01' },
+    { id: 2, name: 'Resume.docx', size: '200 KB', date: '2024-01-02' },
+    { id: 3, name: 'Presentation.pptx', size: '4.5 MB', date: '2024-01-03' }
+]
+      };
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .file-cards {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  
+  .file-card {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 20px;
+    margin: 10px;
+    width: 180px; /* 调整卡片宽度根据需要 */
+  }
+  
+  .file-info {
+    text-align: center;
+  }
+  
+  .file-img {
+    width: 100px; /* 调整图片大小根据需要 */
+    height: 100px; /* 调整图片大小根据需要 */
+    display: block; /* 让图片居中 */
+    margin: 0 auto; /* 让图片居中 */
+  }
+  
+  .file-divider {
+    width: 50%; /* 调整横杠宽度根据需要 */
+    margin: 10px auto; /* 让横杠居中 */
+  }
+  </style>
---
+++ pages/main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <div class="content">
+
+    <h1 class="title">我的文件</h1>
+            <hr class="file-divider"> <!-- 添加横杠 -->
+            <FileList/>
+  </div>
+</div>
+</template>
+
+<script>
+import FileList from "../component/FileList.vue"
+import Navbar from "../component/navbar.vue"; // 根据你的目录结构修改路径
+
+
+export default {
+  name: 'App',
+  components: {
+    FileList,
+    Navbar
+  }
+}
+</script>
+
+<style>
+.title{
+    font-size: 36px; /* 调整标题大小根据需要 */
+    margin-left: 20px; /* 将标题移到页面左侧 */  
+} 
+.content {
+  margin-left: 220px; /* 给导航栏留出空间 */
+  padding: 20px;
+}
+</style>
