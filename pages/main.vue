7da7bc120cf52a4d447966e93c45b185d6a8c654
TianG <18232025994@139.com>
2024-04-22 00:36:15 +0800

    error solution

--- pages/main.vue
+++ pages/main.vue
@@ -24,7 +24,7 @@
       <template #header>
         <div class="flex items-center justify-between">
           <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-            Modal
+            文件信息
           </h3>
           <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
         </div>
